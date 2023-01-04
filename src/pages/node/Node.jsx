488b1d8458340c82f93bb97e0e37f7faffc7acd3
gan <kaeun181818@gmail.com>
2023-01-04 11:50:41 +0900

    리차트 추가

--- src/pages/node/Node.jsx
+++ src/pages/node/Node.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import NodeTable from "./components/NodeTable";
 import NodeTap from "../node/components/NodeTab";
+import NodeDetail from "./NodeDetail";
 
 const Node = () => {
   const [selectedTap, setSelectedTap] = useState("NODE_LIST");
@@ -9,8 +10,11 @@ const Node = () => {
   return (
     // <S.GlobalContainer>
     <>
+      <h1>노드</h1>
+      <p>전체 노드 x개</p>
       <NodeTap selectedTap={selectedTap} setSelectedTap={setSelectedTap} />
       <NodeTable />
+      <NodeDetail />
     </>
     // </S.GlobalContainer>
   );
