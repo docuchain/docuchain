5befea4b83f2c6d96cc795788aab5310ca20ed6c
gan <kaeun181818@gmail.com>
2023-01-06 14:22:10 +0900

    주석

--- src/pages/node/NodeDetail.jsx
+++ src/pages/node/NodeDetail.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 import NodeMemoryChart from "./utils/chart/nodeMemoryChart";
-import NodeRssChart from "./utils/chart/nodeRssChart";
-import NodeUsageChart from "./utils/chart/nodeUsageChart";
+// import NodeRssChart from "./utils/chart/nodeRssChart";
+// import NodeUsageChart from "./utils/chart/nodeUsageChart";
+
+// import { nodeMockData } from "../node/utils/nodeMockData";
 
 const NodeDetail = () => {
   return (
     <div>
-      <h1>node detail page</h1>
+      <h1>상세정보</h1>
+      <h3>nodeMockData.nodeName (nodeMockData.ip)</h3>
+      {/* id가 ${nodeName}인지 한 번 확인하는 코드 넣기
+      props or useParams 사용 해보기 */}
       <NodeMemoryChart />
       {/* <NodeRssChart />
       <NodeUsageChart /> */}
