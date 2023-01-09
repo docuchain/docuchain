4bf863d5338b128cc1d1f7bbd8355913827bfe99
문승현 <anstmdgus473@naver.com>
2023-01-09 13:51:44 +0900

    1월9일 업로드

--- src/pages/service/ServiceDetail.jsx
+++ src/pages/service/ServiceDetail.jsx
@@ -1,54 +1,61 @@
-import React from "react";
-import NodeName from "./utils/nodeName";
-import TransNum from "./utils/transNum";
-import TransHash from "./utils/transHash";
+import React, { useState, useEffect } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 import CopyBtn from "../block/utils/copyBtn";
+import serviceTable from "./utils/serviceTable";
 import ToServiceMainBtn from "./utils/toServiceMainBtn";
-import "./CSS/Service.scss";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
+import { getDocs } from "firebase/firestore";
+import { query } from "firebase/database";
 
 const columns = [
-  { id: "serviceName", label: "서비스명", link: "service/serviceDetail" },
-  { id: "date", label: "타임스탬프", link: "service/serviceDetail" },
-  { id: "ApiKinds", label: "API 종류", link: "service/serviceDetail" },
+  {
+    id: "serviceName",
+    label: "서비스명",
+    minWidth: 10,
+    link: "service/ServiceDetail",
+  },
+  {
+    id: "date",
+    label: "타임스탬프",
+    minWidth: 10,
+    link: "service/serviceDetail",
+  },
+  {
+    id: "ApiKinds",
+    label: "API 종류",
+    minWidth: 0,
+    link: "service/serviceDetail",
+  },
   { id: "NodeName", label: "노드명", minWidth: 10, link: "node/nodeDetail" },
-  { id: "TransNum", label: "트랜잭션번호", link: "trans/transDetail" },
-  { id: "TransHash", label: "트랜잭션해시", link: "trans/transDetail" },
+  {
+    id: "TransNum",
+    label: "트랜잭션번호",
+    minWidth: 10,
+    link: "trans/transDetail",
+  },
+  {
+    id: "TransHash",
+    label: "블록번호",
+    minWidth: 0,
+    link: "block/blockDetail",
+  },
   { id: "BlockNum", label: "블록번호", minWidth: 0, link: "block/blockDetail" },
   { id: "state", label: "상태", minWidth: 0 },
 ];
 
-function createData(
-  id,
-  ServiceName,
-  date,
-  ApiKinds,
-  NodeNum,
-  TransNum,
-  TransHash,
-  BlockNum,
-  state
-) {
-  return {
-    id,
-    ServiceName,
-    date,
-    ApiKinds,
-    NodeNum,
-    TransNum,
-    TransHash,
-    BlockNum,
-    state,
-  };
-}
-
 const ServiceDetail = () => {
-  const [data, setData] = useState([]);
+  // 데이터 담기
+  const [data, setData] = React.useState([]);
+  //데이터 불러오기
   const fetchdata = async () => {
     try {
       const res = await fetch(
-        "https://docuchain-72799-default-rtdb.asia-southeast1.firebasedatabase.app/arr.json"
+        "https://docuchain-72799-default-rtdb.asia-southeast1.firebasedatabase.app/Service.json"
       );
       const result = await res.json();
       setData([...result]);
@@ -56,37 +63,59 @@ const ServiceDetail = () => {
       console.log(error);
     }
   };
+  //fetchdata firebase data
   React.useEffect(() => {
     fetchdata();
   }, []);
 
+  // ===============================================
+
   return (
-    <div className="serviceDetail">
-      <h2>서비스</h2>
-      <h3>| 상세정보</h3>
-
-      {/* 버튼(목록으로) */}
-      <ToServiceMainBtn />
-
-      <div>
-        {columns.map((column) => (
-          <h5 key={column.id}>{column.label}</h5>
-        ))}
-      </div>
-
-      {/* <h5>네트워크명 {serviceName}</h5>
-      <h5>타임스탬프 {date}</h5>
-      <h5>API 종류 {ApiKinds}</h5>
-      <h5 className="NodeName">노드명 {nodeName}</h5>
-      <h5 className="TransName">
-        {transName}
-        <span>
-          <CopyBtn />
-        </span>
-      </h5>
-      <h5>트랜잭션해시 {transHash}</h5>
-      <h5>블록번호 {BlockNum}</h5>
-      <h5>상태 {state}</h5> */}
+    <div className="blockDetailTable">
+      <TableContainer component={Paper}>
+        <div className="serviceHome--Btn">
+          <ToServiceMainBtn />
+        </div>
+        <Table sx={{ minWidth: 600 }} aria-label="simple table">
+          <TableBody>
+            <TableRow>
+              <TableCell>네트워크명</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>타임스탬프</TableCell>
+              <TableCell>data</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>API 종류</TableCell>
+              <TableCell>data</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>노드명</TableCell>
+              <TableCell>data</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>트랜잭션번호</TableCell>
+              <TableCell>data</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>트랜잭션해시</TableCell>
+              <TableCell>data</TableCell>
+              <TableCell>
+                <CopyBtn />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>블록번호</TableCell>
+              <TableCell>data</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>상태</TableCell>
+              <TableCell>data</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
--- src/pages/service/utils/serviceTable.js
+++ src/pages/service/utils/serviceTable.js
@@ -1,4 +1,5 @@
-import * as React from "react";
+// import * as React from "react";
+import React, { useState, useEffect } from "react";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -7,7 +8,8 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import ServiceDetail from "../ServiceDetail";
 
 const ConditionalLink = ({ children, condition, ...props }) => {
   return !!condition && props.to ? (
@@ -22,13 +24,13 @@ const columns = [
     id: "serviceName",
     label: "서비스명",
     minWidth: 10,
-    link: "service/serviceDetail",
+    link: `service/ServiceDetail/`,
   },
   {
     id: "date",
     label: "타임스탬프",
     minWidth: 10,
-    link: "service/serviceDetail",
+    link: "service/serviceDetail/",
   },
   {
     id: "ApiKinds",
@@ -47,8 +49,10 @@ const columns = [
   { id: "state", label: "상태", minWidth: 0 },
 ];
 
-export default function StickyHeadTable() {
+export default function StickyHeadTable(prpos) {
+  // 데이터 담기
   const [data, setData] = React.useState([]);
+  //데이터 불러오기
   const fetchdata = async () => {
     try {
       const res = await fetch(
@@ -60,6 +64,7 @@ export default function StickyHeadTable() {
       console.log(error);
     }
   };
+  //fetchdata firebase data
   React.useEffect(() => {
     fetchdata();
   }, []);
@@ -76,6 +81,17 @@ export default function StickyHeadTable() {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
+
+  const [serviceId, setServiceId] = useState("");
+  const navigate = useNavigate();
+  const toServiceDetail = (e) => {
+    console.log(e.target.value);
+    setServiceId(e.target.value);
+  };
+
+  useEffect(() => {
+    navigate(`/service/${serviceId}`);
+  }, [serviceId]);
 
   return (
     <Paper sx={{ width: "100%", overflow: "hidden" }}>
@@ -94,16 +110,17 @@ export default function StickyHeadTable() {
           <TableBody>
             {data
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
+              .map((data) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  <TableRow hover role="checkbox" tabIndex={-1} key={data.code}>
                     {columns.map((column) => {
-                      const value = row[column.id];
+                      const value = data[column.id];
                       //메인 테이블
                       return (
                         <TableCell key={column.id} align={column.align}>
                           {/* condition (조건문) 일치하는 것만 링크 */}
                           <ConditionalLink
+                            onclick={toServiceDetail}
                             to={`/${column.link}`}
                             condition={column.link !== undefined}
                           >
@@ -132,4 +149,9 @@ export default function StickyHeadTable() {
       />
     </Paper>
   );
+}
+
+// const handleClick = (e) => console.log(e.target.id);
+{
+  /* <ConditionalLink onClick={handleClick} */
 }
