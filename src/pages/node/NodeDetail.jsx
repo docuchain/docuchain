e6e3874d50f332f31d0c4f26c8582f3d5798e3f3
gan <kaeun181818@gmail.com>
2023-01-10 17:15:00 +0900

    firebase 노드전체 차트그리기

--- src/pages/node/NodeDetail.jsx
+++ src/pages/node/NodeDetail.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { nodeMockData } from "../node/utils/nodeMockData";
 import NodeMemoryChart from "./utils/chart/nodeMemoryChart";
-// import NodeRssChart from "./utils/chart/nodeRssChart";
+import NodeRssChart from "./utils/chart/nodeRssChart";
 // import NodeUsageChart from "./utils/chart/nodeUsageChart";
 
 const NodeDetail = () => {
@@ -30,10 +30,13 @@ const NodeDetail = () => {
         {nodeName} : {NodeIP}
       </h3>
       <button onClick={toNode}>목록으로</button>
+      {/* https://speed.cloudflare.com/
+      사이트에 접속하는 것만으로 측정
+      통신 지연 이나 IP나 서버 정보, 용량별 다운로드 속도 등 상세한 정보 제공 */}
 
       <NodeMemoryChart />
-      {/* <NodeRssChart />
-      <NodeUsageChart /> */}
+      <NodeRssChart />
+      {/* <NodeUsageChart /> */}
     </div>
   );
 };
