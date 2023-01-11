35cf642aefc05f9fc5f847bb66be11c47932b356
문승현 <anstmdgus473@naver.com>
2023-01-11 10:52:55 +0900

    footer style

--- src/pages/service/utils/serviceTable.js
+++ src/pages/service/utils/serviceTable.js
@@ -10,7 +10,9 @@ import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import { Link, useNavigate } from "react-router-dom";
 import ServiceDetail from "../ServiceDetail";
-
+import { useRecoilValue } from "recoil";
+import { getUserInfo } from "../../../recoil/selector";
+import swal from "sweetalert";
 const columns = [
   { id: "serviceName", label: "서비스명" },
   { id: "date", label: "타임스탬프" },
@@ -22,6 +24,28 @@ const columns = [
 ];
 
 export default function StickyHeadTable() {
+  const userValue = useRecoilValue(getUserInfo);
+  //서비스 권한여부 판별
+  const serviceAuth = (e) => {
+    if (!userValue.service) {
+      swal("", "권한이 없습니다. 관리자에게 문의하십시오", "error");
+      e.preventDefault();
+    }
+  };
+  //노드 권한여부 판별
+  const nodeAuth = (e) => {
+    if (!userValue.node) {
+      swal("", "권한이 없습니다. 관리자에게 문의하십시오", "error");
+      e.preventDefault();
+    }
+  };
+  //트랜잭션 권한여부 판별
+  const transAuth = (e) => {
+    if (!userValue.trans) {
+      swal("", "권한이 없습니다. 관리자에게 문의하십시오", "error");
+      e.preventDefault();
+    }
+  };
   // 데이터 담기
   const [data, setData] = React.useState([]);
   //데이터 불러오기
@@ -40,6 +64,8 @@ export default function StickyHeadTable() {
   React.useEffect(() => {
     fetchdata();
   }, []);
+
+  //==================================================
 
   const [page, setPage] = React.useState(0);
   //Rows per page 단위
@@ -80,25 +106,41 @@ export default function StickyHeadTable() {
                     key={data.transCount}
                   >
                     <TableCell>
-                      <Link to={`${data.transCount}`} value={data.serviceName}>
+                      <Link
+                        to={`${data.transCount}`}
+                        value={data.serviceName}
+                        onClick={serviceAuth}
+                      >
                         {data.serviceName}
                       </Link>
                     </TableCell>
                     <TableCell>
-                      <Link to={`${data.transCount}`} value={data.serviceName}>
+                      <Link
+                        to={`${data.transCount}`}
+                        value={data.serviceName}
+                        onClick={serviceAuth}
+                      >
                         {data.timeStamp}
                       </Link>
                     </TableCell>
                     <TableCell>
-                      <Link to={`${data.transCount}`} value={data.serviceName}>
+                      <Link
+                        to={`${data.transCount}`}
+                        value={data.serviceName}
+                        onClick={serviceAuth}
+                      >
                         {data.apiKinds}
                       </Link>
                     </TableCell>
                     <TableCell>
-                      <Link to={"node/nodeDetail"}>{data.nodeName}</Link>
+                      <Link to={"node/nodeDetail"} onClick={nodeAuth}>
+                        {data.nodeName}
+                      </Link>
                     </TableCell>
                     <TableCell>
-                      <Link to={"/trans/transDetail"}>{data.transNumber}</Link>
+                      <Link to={"/trans/transDetail"} onClick={transAuth}>
+                        {data.transNumber}
+                      </Link>
                     </TableCell>
                     <TableCell>
                       <Link to={"/block/blockDetail"}>{data.newBlockNum}</Link>
