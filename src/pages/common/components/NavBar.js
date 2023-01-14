7a48f9902eee6d7da28f5ad4208c18001558e663
문승현 <anstmdgus473@naver.com>
2023-01-15 01:25:56 +0900

    메뉴바 선택시 색상변경

--- src/pages/common/components/NavBar.js
+++ src/pages/common/components/NavBar.js
@@ -9,10 +9,14 @@ const NavBar = () => {
   const nowUser = authService.currentUser;
   const userValue = useRecoilValue(getUserInfo);
   const navigate = useNavigate();
-  const activeStyle = {
-    background: "#4e81ef",
-    color: "yellow",
-  };
+
+  // =============================
+
+  // let data = ["대시보드", "블록", "트랜잭션", "노드", "서비스"];
+
+  const [tab, setTab] = useState("");
+
+  // ==========================
 
   return (
     <div className="Navbar">
@@ -20,7 +24,7 @@ const NavBar = () => {
       <div className="logo">
         <div className="img" onClick={() => navigate("/")}></div>
       </div>
-      {/* 영역 2 */}
+      ;{/* 영역 2 */}
       <div className="innerContainer">
         <div className="avatar">
           <div className="img"></div>
@@ -29,59 +33,40 @@ const NavBar = () => {
         </div>
         <div className="nav">
           <ul className="navList">
-            <li className="NavBar__link">
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  isActive ? "navlinkClicked" : "navlinkDefault"
-                }
-              >
-                대시보드
-              </NavLink>
+            <li
+              className={`li ${tab === "dash" ? "navlinkClicked" : ""}`}
+              onClick={() => setTab("dash")}
+            >
+              <NavLink to="/">대시보드</NavLink>
             </li>
-            <li className="NavBar__link">
-              <NavLink
-                to="/block"
-                className={({ isActive }) =>
-                  isActive ? "navlinkClicked" : "navlinkDefault"
-                }
-              >
-                블록
-              </NavLink>
+            <li
+              className={`li ${tab === "block" ? "navlinkClicked" : ""}`}
+              onClick={() => setTab("block")}
+            >
+              <NavLink to="/block">블록</NavLink>
             </li>
-            <li className="NavBar__link">
-              <NavLink
-                to="/trans"
-                className={({ isActive }) =>
-                  isActive ? "navlinkClicked" : "navlinkDefault"
-                }
-              >
-                트랜잭션
-              </NavLink>
+            <li
+              className={`li ${tab === "trans" ? "navlinkClicked" : ""}`}
+              onClick={() => setTab("trans")}
+            >
+              <NavLink to="/trans">트랜잭션</NavLink>
             </li>
-            <li className="NavBar__link">
-              <NavLink
-                to="/node"
-                className={({ isActive }) =>
-                  isActive ? "navlinkClicked" : "navlinkDefault"
-                }
-              >
-                노드
-              </NavLink>
+            <li
+              className={`li ${tab === "node" ? "navlinkClicked" : ""}`}
+              onClick={() => setTab("node")}
+            >
+              <NavLink to="/node">노드</NavLink>
             </li>
-            <li className="NavBar__link">
-              <NavLink
-                to="/service"
-                className={({ isActive }) =>
-                  isActive ? "navlinkClicked" : "navlinkDefault"
-                }
-              >
-                서비스
-              </NavLink>
+            <li
+              className={`li ${tab === "service" ? "navlinkClicked" : ""}`}
+              onClick={() => setTab("service")}
+            >
+              <NavLink to="/service">서비스</NavLink>
             </li>
           </ul>
         </div>
       </div>
+      ;
     </div>
   );
 };
